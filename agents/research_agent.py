b327fa80cff4b24473f2407a12e662da86072348
Tharun Teja Peddi <tharunteja2009@gmail.com>
2025-07-20 23:12:35 +0800

    changes to research agent

---
+++ agents/research_agent.py
@@ -0,0 +1,26 @@
+from autogen_agentchat.agents import AssistantAgent
+from models import open_ai_client
+from langchain_community.utilities import GoogleSerperAPIWrapper
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+google_search = GoogleSerperAPIWrapper(type="search", num_results=3)
+os.environ["SERPER_API_KEY"] = os.getenv("SERPER_API_KEY")
+
+
+def web_search_tool(query: str) -> str:
+    """Query must be a string"""
+    return google_search.run(query)
+
+
+def do_research():
+    agent = AssistantAgent(
+        name="web_search_agent",
+        description="An agent that performs web searches to answer questions.",
+        model_client=open_ai_client.get_model_client(),
+        system_message="You are a research agent that can search the web to find information. Use the web search tool to answer questions. in 100 words",
+        tools=[web_search_tool],
+    )
+    return agent
--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 import asyncio
-from agents import chat_agent
+from agents import research_agent
 from autogen_agentchat.messages import TextMessage
 
 
 async def main():
-    chat_agent_instance = chat_agent.get_chat_agent()
-    # result = await chat_agent_instance.run(task="Who are you")
-
-    stream = chat_agent_instance.run_stream(
-        task="Give me top 5 Indian EV cars, with brandnames, battery, range, features, price. in short bullet points.",
+    chat_agent_instance = research_agent.do_research()
+    result = await chat_agent_instance.run(
+        task="How was latest quarterly earnings for HDFC Bank?"
     )
-
-    async for message in stream:
-        if isinstance(message, TextMessage):
-            print(f"[{message.source}] {message.content}\n")
+    print("Result:", result)
 
 
 if __name__ == "__main__":
